decdb87d8dd2e8698ca552e794963103cd14779a
ojhdream <ojhdream@gmail.com>
2026-06-30 12:27:22 +0900

    Fix PWA stale shell cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'maeumtuk2-pwa-v2'
+const CACHE_NAME = 'maeumtuk2-pwa-v3'
 const APP_SHELL = [
   './',
   './index.html',
@@ -31,6 +31,19 @@ self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return
   const requestUrl = new URL(event.request.url)
   if (requestUrl.origin !== self.location.origin) return
+
+  if (event.request.mode === 'navigate' || event.request.headers.get('accept')?.includes('text/html')) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy))
+          return response
+        })
+        .catch(() => caches.match('./index.html')),
+    )
+    return
+  }
 
   event.respondWith(
     caches.match(event.request).then((cached) => {
