c8ce46f95b05438c7d98c1cf367d128a241277da
ojhdream <ojhdream@gmail.com>
2026-07-02 11:15:29 +0900

    Refine brand mark

--- prototype-src/src/components/Brand.jsx
+++ prototype-src/src/components/Brand.jsx
@@ -3,11 +3,12 @@ export default function Brand({ action, onAction }) {
     <div className="topbar">
       <div className="brand">
         마음<span className="brand-tuk">툭</span>
-        <span className="mark">
+        <span className="mark refined-mark" aria-hidden="true">
           <i className="line"></i>
           <i className="dot"></i>
         </span>
       </div>
+
       <button type="button" className="icon-btn" onClick={onAction}>
         {action}
       </button>
